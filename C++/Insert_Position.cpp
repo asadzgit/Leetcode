dc622ccadf3e413bec0d60c841455ba4141a2f23
Muhammad Asad <66944583+asadzgit@users.noreply.github.com>
2020-10-02 22:01:20 +0500

    Update Insert_Position.cpp

--- C++/Insert_Position.cpp
+++ C++/Insert_Position.cpp
@@ -21,17 +21,17 @@
 //    Input : [1, 3, 5, 6] , 0
 //    Output : 0
 //**************************************************************************************************************
-//class Solution {
-//public:
-//    int searchInsert(vector<int>& nums, int target) {
-//        int i = 0;
-//        if (target < nums[0])
-//            return 0;
-//        for (; i < nums.size(); i++) {
-//            if (target == nums[i] || target < nums[i]) {
-//                return i;
-//            }
-//        }
-//        return i;
-//    }
-//};
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0;
+        if (target < nums[0])
+            return 0;
+        for (; i < nums.size(); i++) {
+            if (target == nums[i] || target < nums[i]) {
+                return i;
+            }
+        }
+        return i;
+    }
+};
